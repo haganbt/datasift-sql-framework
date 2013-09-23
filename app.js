f392d3d0ecf5ef641f4d533f9e0ac69d85d4e86e
Ben Hagan <benhagan@gmail.com>
2013-09-23 20:12:03 +0100

    Basic ORM implementation.

--- app.js
+++ app.js
@@ -1,8 +1,53 @@
+// TODO - configure orm as middleware.
+
 'use strict';
 var express = require('express')
   , http = require('http')
   , path = require('path')
+  , orm = require("orm")
   ;
+
+// TODO: extract to config file
+var opts = {
+  database : "datasift",
+  protocol : "mysql",
+  host     : "127.0.0.1",
+  port     : 3306,         // optional, defaults to database default
+  user     : "root",
+  password : "",
+  query    : {
+    pool     : false,    // optional, false by default
+    debug    : false    // optional, false by default
+  }
+};
+
+
+// Database
+orm.connect(opts, function (err, db) {
+  if (err) {
+    console.log("Failed to connect to DB: ", err);
+    return;
+  }
+
+  // Load models
+  db.load("./lib/models/models", function (err) {  
+    if (err) {
+        console.log("Cannot load models", err);
+    }
+    // loaded!
+    var Person = db.models.person;
+    var Pet    = db.models.pet;
+    
+    // Drop and recreate schema
+    db.drop(function () {
+        // dropped all tables from defined models (Person and Pet)
+      Person.sync(function () {
+        // created tables for Person model
+      });
+    });
+  });
+});
+
 
 var app = express();
 
@@ -13,13 +58,9 @@ app.configure(function () {
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.limit('20mb'));
-
-  /* 
-   * NOTE: DataSift sends application/json header
-   * although line delmited is not valid JSON. 
-   * This breaks bodyParser middleware. Pull the
-   * body manually.
-   */ 
+  // Note we cannot use bodyParser as DS
+  // seds a application/json header for line
+  // delimited data - strictly invalid
 	app.use (function(req, res, next) {
 	    var data='';
 	    req.setEncoding('utf8');
