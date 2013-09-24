0afbe5e92f636be10412aa5fe56fb76b7667b3e0
Ben Hagan <benhagan@gmail.com>
2013-09-24 15:48:51 +0100

    Basic support for interaction table.

--- app.js
+++ app.js
@@ -1,60 +1,8 @@
-// TODO - configure orm as middleware.
-
 'use strict';
 var express = require('express')
   , http = require('http')
   , path = require('path')
-  , orm = require("orm")
   ;
-
-// TODO: extract to config file
-var opts = {
-  database : "datasift",
-  protocol : "mysql",
-  host     : "127.0.0.1",
-  port     : 3306,         // optional, defaults to database default
-  user     : "root",
-  password : "",
-  query    : {
-    pool     : false,    // optional, false by default
-    debug    : false    // optional, false by default
-  }
-};
-
-
-// Database
-orm.connect(opts, function (err, db) {
-  if (err) {
-    console.log("Failed to connect to DB: ", err);
-    return;
-  }
-
-  // Globally set interaction id on all tables
-  db.settings.set("properties.primary_key", "interaction_id");
-  
-  // Load models
-  db.load("./lib/models/models", function (err) {  
-    if (err) {
-      console.log("Failed to load models: ", err);
-    }
-    // loaded!
-    var Interaction = db.models.interaction;
-    var Tags    = db.models.tags;
-    
-    // Drop and recreate schema
-    db.drop(function () {
-        // dropped all tables from defined models
-      Interaction.sync(function () {
-        // created tables for interaction model
-      });
-      
-      Tags.sync(function () {
-        // created tables for tags model
-      });
-    });
-  });
-});
-
 
 var app = express();
 
@@ -65,8 +13,9 @@ app.configure(function () {
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.limit('20mb'));
-  // Note we cannot use bodyParser as DS
-  // seds a application/json header for line
+	
+  // NOTE: we cannot use bodyParser as DS
+  // sends a application/json header for line
   // delimited data - strictly invalid
 	app.use (function(req, res, next) {
 	    var data='';
