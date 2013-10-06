951feef7576882564d5ed959a945e5e672bc2ff1
Ben Hagan <benhagan@gmail.com>
2013-10-06 18:01:44 -0400

    clean up

--- extension/test/model.js
+++ extension/test/model.js
@@ -32,25 +32,5 @@ module.exports = function (db, cb) {
     text                    : { type: "text", size: 256 }
   });
   
-/*
- 	// TAGS
-  db.define('tags', {
-    id      		: { type: "text", size: 64 },
-    created_at  : Date,    
-    tag     		: { type: "text", size: 128 }
-    }, {
-      id: 'tag_id' // auto generate a UUID as the default interaction.id here is not unique
-  });
-
-  // LINKS
-  db.define('links', {
-    id      		: { type: "text", size: 64 },
-    created_at  : Date,    
-    title   		: { type: "text", size: 250 },
-    url     		: { type: "text", size: 250 }
-    }, {
-      id: 'links_id' // auto generate a UUID as the default interaction.id here is not unique
-  });
- */  
   return cb();
 };
